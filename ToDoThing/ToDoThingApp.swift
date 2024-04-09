4942691a78169b9912595a5b0c29a24fc197b903
Lexi Harrop <lharrop@lcs.on.ca>
2024-04-09 10:57:01 -0400

    Initial Commit

---
+++ ToDoThing/ToDoThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoThingApp.swift
+//  ToDoThing
+//
+//  Created by Lexi on 2024-04-09.
+//
+
+import SwiftUI
+
+@main
+struct ToDoThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
